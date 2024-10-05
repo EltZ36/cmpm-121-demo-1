7ef85f192e2deb36afdd751999f924281655321a
Elton Z <poptako9@gmail.com>
2024-10-04 18:08:29 -0700

    main/Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,11 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+//button code pulled from the lecture on 10/02/24
+const button = document.createElement('button');
+button.innerHTML = "🪤"
+app.append(button)
+
+
+
