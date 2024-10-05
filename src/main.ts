f4fa67ae1e1073c8455a271faf2d60b2db5002bf
Elton Z <poptako9@gmail.com>
2024-10-04 18:33:22 -0700

    main/Step 2 Complete

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
+const counterValue: HTMLDivElement = document.querySelector("#counterValue")!;
 
 const gameName = "My clicking game";
 document.title = gameName;
@@ -9,10 +10,14 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-//button code pulled from the lecture on 10/02/24
-const button = document.createElement('button');
-button.innerHTML = "🪤"
-app.append(button)
+//button code pulled from the lecture on 10/02/24 and mdn docs for addEventListener
+let counter: number = 0; 
+const button = document.createElement("button");
+button.innerHTML = "🪤";
+button.addEventListener("click" , () => {
+    counter += 1;
+    counterValue.innerHTML = `${counter} pieces of cheese stolen`
+});
 
-
-
+app.append(button); 
+app.append(counterValue);
